649e3bc55b7f6dfd6a9b32a3cd4db3db91c361cc
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-05-10 10:13:43 +0500

    uzgarish

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Model from  'react-modal'
 import "./Users.css"
 import male from "../../assets/male-avatar-boy-face-man-user-9.svg"
 import female from "../../assets/female-avatar-girl-face-woman-user-2.svg"
 import  { useDispatch } from 'react-redux'
 import {removFromUser } from '../../context/userSlick'
+import { NavLink } from 'react-router-dom'
+import Confetti from 'react-confetti'
 
 function Users({data}) {
+  const [modul,setModul] =useState(false)
   const dispatch = useDispatch()
+  const [confitte,setConfitee] = useState(false)
   return (
+  <>
+ 
     <div className='users__wrapper'>
       {
 data?.map(user => (
@@ -17,11 +24,60 @@ data?.map(user => (
             <h2>{user.name}</h2>
             <p>{user.position}</p>
             <p>{user.age} years old</p>
-            <button onClick={() =>dispatch(removFromUser(user))}>Remove</button>
+           <div className="hammasi">
+           <button onClick={() =>dispatch(removFromUser(user))}>Remove</button>
+            <button onClick={() =>setModul(true) || setConfitee(!confitte)}>Modul Box</button>
+            {
+              confitte ?
+              <Confetti/>
+              :
+              <></>
+            }
+           </div>
         </div>
 ))
 }
     </div>
+
+    <Model 
+    isOpen={modul}
+     onRequestClose={() =>setModul(false)}
+     style={{
+      overlay:{
+        background:'rgba(255,255,255',
+      },
+      content:{
+     width: '320px',
+     padding: '20px',
+     boxShadow: '0 0 8px #0002, 15px 15px 15px #0001',
+     lineHeight:" 1.6",
+     borderRadius: "10px",
+     marginLeft: "40%",
+      }
+      
+     }}>
+    {
+data?.map(user => (
+  <div className="column">
+    <div className="users__card" key={user.id} >
+            <img src={ user.gender ==="male" ? male : female} alt="" />
+            <h2>{user.surname}</h2>
+            <h2>{user.name}</h2>
+            <p>{user.position}</p>
+            <p>{user.age} years old</p>
+           <div className="hammasi">
+          <NavLink to={'/'}> <button>Remove</button></NavLink>
+            <button onClick={() =>setModul(false) || setConfitee(!confitte)}>Delet</button>
+          
+           </div>
+        </div>
+  </div>
+))
+}
+    </Model>
+  </>
+
+    
   )
 }
 
